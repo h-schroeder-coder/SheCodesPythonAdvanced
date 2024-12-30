b6af84c899b0f57c6be1fb06ce3bdc60d766d4c5
h-schroeder-coder <hankorschroeder@gmail.com>
2024-12-30 12:40:17 -0600

    Created Week 1 lesson 3
    Create a new repl
    Define a class User which prints “Hello from the User class” when it’s initilizing
    Create 3 instances of that class and store them in different variables

---
+++ SheCodesPythonAdvanced_Workshop_Week_1_Lesson_3.py
@@ -0,0 +1,9 @@
+class User: 
+    
+    def __init__(self) :
+        """Initializes a new user"""
+        print("Hello from the User class")
+        
+tessa = User()
+tatum = User()
+elmer = User()
