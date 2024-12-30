cd6a46aaed8170011573c50b9cfb34dd37e7ce47
h-schroeder-coder <hankorschroeder@gmail.com>
2024-12-30 14:05:35 -0600

    Updates to lesson 4

--- SheCodesPythonAdvanced_Workshop_Week_1_Lesson_4.py
+++ SheCodesPythonAdvanced_Workshop_Week_1_Lesson_4.py
@@ -1,15 +1,15 @@
 #Define a class User who receives 2 attributes in the initializer, a first name, and a last name.
 class User: 
-    def __init__(self, firstname, lastname):
+    def __init__(self, first_name, last_name):
         """Initialize the user"""
-        self.firstname = firstname
-        self.lastname = lastname
+        self.first_name = first_name
+        self.last_name = last_name
 
 #Create a method called full_name, which returns the user's full name
     def full_name(self):
         """Returns a full name"""
-        print(f"{self.firstname} {self.lastname}")
-
+        return f"{self.first_name} {self.last_name}"
+      
 
 #Create 2 users with 2 different names and print their full name
 tessa = User("Tessa", "Lynn")
@@ -18,6 +18,7 @@ hadley = User("Hadley", "Jo")
 
 #Change the first name of the first object and print the full name again
 
-tessa.full_name()
-elmer.full_name()
-hadley.full_name()
+print(tessa.full_name())
+print(elmer.full_name())
+print(hadley.full_name())
+
