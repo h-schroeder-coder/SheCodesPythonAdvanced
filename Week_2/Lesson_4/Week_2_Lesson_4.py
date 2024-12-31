c1ebe570a7490c72e2094bbb55f765d81efb2b93
h-schroeder-coder <hankorschroeder@gmail.com>
2024-12-31 09:04:58 -0600

    Adjustment to line break

--- Week_2/Lesson_4/Week_2_Lesson_4.py
+++ Week_2/Lesson_4/Week_2_Lesson_4.py
@@ -1,5 +1,5 @@
 with open('week_2/lesson_4/cities.csv', 'a') as file:
-    file.write('\nFall Creek,29,overcast clouds\n')
-    file.write('Thorp,27,partially sunny')
+    file.write('\nFall Creek,29,overcast clouds')
+    file.write('\nThorp,27,partially sunny')
     
     file.close()
