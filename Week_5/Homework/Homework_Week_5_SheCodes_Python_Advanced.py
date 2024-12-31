267123f85cd26bf09feef3a5f82a33bb8e227e9a
h-schroeder-coder <hankorschroeder@gmail.com>
2024-12-31 17:00:11 -0600

    Adding visualization and style to fake plots

--- Week_5/Homework/Homework_Week_5_SheCodes_Python_Advanced.py
+++ Week_5/Homework/Homework_Week_5_SheCodes_Python_Advanced.py
@@ -1,5 +1,7 @@
 import csv
 
+import matplotlib.pyplot as plt
+
 filename = 'Week_5\Homework\data.csv'
 
 with open(filename, 'r') as csvfile:
@@ -10,4 +12,11 @@ with open(filename, 'r') as csvfile:
     population = line['population']
     
     
- 
+    
+plt.plot([1990, 2000, 2010, 2020], [0, 25000000, 30000000, 35000000], label="Population", marker='*', color='m')
+plt.plot([10, 20, 30], [15000000, 30000000, 40000000], label ="Animal", marker='o', color='c')
+plt.xlabel("Year")
+plt.ylabel("Population")
+plt.title("Populations Across Continents & Time")
+plt.grid(True)
+plt.show()
