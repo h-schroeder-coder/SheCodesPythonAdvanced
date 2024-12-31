71288cae5c00c41222dd96dd4368664a585fd3d8
h-schroeder-coder <hankorschroeder@gmail.com>
2024-12-31 16:45:55 -0600

    updated file paths

--- Week_5/Homework/Homework_Week_5_SheCodes_Python_Advanced.py
+++ Week_5/Homework/Homework_Week_5_SheCodes_Python_Advanced.py
@@ -1,7 +1,9 @@
 import csv
 
-filename = 'data.csv'
+filename = 'Week_5\Homework\data.csv'
 
 with open(filename, 'r') as csvfile:
-  reader - csv.DictReader(csvfile)
-  
+  reader = csv.DictReader(csvfile)
+  for line in reader:
+    print(line['continent'])
+ 
