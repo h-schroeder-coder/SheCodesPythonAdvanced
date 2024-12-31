90f2f3ea9b44863256b48b2c9eb0049b763d620f
h-schroeder-coder <hankorschroeder@gmail.com>
2024-12-31 14:31:22 -0600

    Created Week 4 lesson 3

---
+++ Week_4/Lesson_3/Week_4_Lesson_3.py
@@ -0,0 +1,44 @@
+temperatures = [10, 12, 14, 15]
+
+# Print the list
+print(temperatures)
+
+# Modify an existing temperature
+temperatures[2] = 17
+
+
+
+# Print the modified item
+print(temperatures[2])
+
+# Add a temperature to the list
+temperatures.append(18)
+
+# Print the the newly added item
+print(temperatures)
+print(temperatures[4])
+
+forecast = {
+  "Monday": { "temperature": 21, "condition": "Rainy"},
+  "Tuesday": { "temperature": 20, "condition": "Sunny"},
+  "Wednesday": { "temperature": 23, "condition": "Cloudy"},
+  "Thursday": { "temperature": 24, "condition": "Sunny"},
+}
+
+# Print the dictionary
+print(forecast)
+
+# Modify Wednesdays temperature to 25 and Sunny
+forecast["Wednesday"]["temperature"] = 25
+forecast["Wednesday"]["condition"] = "Sunny"
+
+# Print Wednesdays temperature
+print(forecast['Wednesday']['temperature'])
+
+# Add forecast for Friday, 27, Cloudy
+forecast['Friday'] = { "temperature": 27, "condition": "Cloudy"}
+print(forecast['Friday'])
+
+# Print Friday temperature such as "Friday's temperature will be 27 degrees and cloudy
+print(f"Friday's temperature will be {forecast['Friday']['temperature']} degrees and {forecast['Friday']["condition"]}")
+      
