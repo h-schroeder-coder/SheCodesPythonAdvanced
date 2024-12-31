2b82feb1105160efde69f6a16225f003796c6cea
h-schroeder-coder <hankorschroeder@gmail.com>
2024-12-31 08:46:55 -0600

    Showing alternative methods

--- Week_2/Lesson_3/Week_2_Lesson_3.py
+++ Week_2/Lesson_3/Week_2_Lesson_3.py
@@ -4,9 +4,18 @@ with open('week_2/lesson_3/temperatures.csv', 'r') as file:
     csv_reader = csv.reader(file)
     
     for city in csv_reader:
-        print("----")
+        #option 1
+        print("-------")
+        print(f"It is {city[2].lower()} and {city[1]}ºC in {city[0]}")
+        ##
+        print("-------")
         print(f"City: {city[0]}")
         print(f"Temperature: {city[1]}")
         print(f"Conditions: {city[2]}")
-        print(f"It is {city[2]} and {city[1]}ºC in {city[0]}")
+        ##
+        #option 2
+        city = city[0]
+        temperature = city[1]
+        condition = city[2].lower()
+        print(f"It is currently {temperature}ºC and {condition} in {city}.")
         print("-------")
