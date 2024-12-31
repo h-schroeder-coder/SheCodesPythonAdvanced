9b1ca026c223a3202887122a8898675128e88404
h-schroeder-coder <hankorschroeder@gmail.com>
2024-12-31 17:36:02 -0600

    Cleaning up code and placing methods into functions

--- Week_5/Homework/Homework_Week_5_SheCodes_Python_Advanced.py
+++ Week_5/Homework/Homework_Week_5_SheCodes_Python_Advanced.py
@@ -2,38 +2,55 @@ import csv
 
 import matplotlib.pyplot as plt
 
+def generate_population_dictionary_from_csv(filename):
+  """
+  Generate population dictionary from csv data and return a dictionary following the structure below
+  {
+    "Africa": { population: [100, 200, 300], years: [1990, 2000, 2010]}
+    "Europe": { population: [100, 200, 300], years: [1990, 2000, 2010]}
+  }
+  """
+  
+  output = {}
+  
+  with open(filename, 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for line in reader:
+      continent = line['continent']
+      year = int(line['year'])
+      population = int(line['population'])
+      
+      if continent not in output:
+        output[continent] = {'population': [], 'years': []}
+      
+      output[continent]['population'].append(population)
+      output[continent]['years'].append(year)
+      
+    return output
+  
+  
+def generate_population_plots_from_dictionary(population_dictionary):
+  """
+  Generate the population plots from a dictionary with one plot per continent
+  """
+  for continent in population_dictionary:
+    years = population_dictionary[continent]['years']
+    population = population_dictionary[continent]['population']
+    plt.plot(years, population, label=continent, marker='*')
+
+  plt.xlabel("Year")
+  plt.ylabel("Population (in billion users)")
+  plt.title("Internet Populations Across Continents & Time")
+  plt.grid(True)
+  plt.legend()
+  plt.tight_layout()
+  plt.show()
+
+
 filename = 'Week_5\Homework\data.csv'
 
-population_per_continent = {}
+# Display internet population in plots
+population_per_continent = generate_population_dictionary_from_csv(filename)
+generate_population_plots_from_dictionary(population_per_continent)
+   
 
-
-
-with open(filename, 'r') as csvfile:
-  reader = csv.DictReader(csvfile)
-  for line in reader:
-    continent = line['continent']
-    year = int(line['year'])
-    population = int(line['population'])
-    
-    if continent not in population_per_continent:
-      population_per_continent[continent] = {'population': [], 'years': []}
-    
-    population_per_continent[continent]['population'].append(population)
-    population_per_continent[continent]['years'].append(year)
-    
-
-for continent in population_per_continent:
-  years = population_per_continent[continent]['years']
-  population = population_per_continent[continent]['population']
-  plt.plot(years, population, label=continent, marker='*')
-  
-  
-    
-
-plt.xlabel("Year")
-plt.ylabel("Population (in billion users)")
-plt.title("Internet Populations Across Continents & Time")
-plt.grid(True)
-plt.legend()
-plt.tight_layout()
-plt.show()
