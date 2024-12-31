1368ab6eff5063282b42669488f963545267fa9e
h-schroeder-coder <hankorschroeder@gmail.com>
2024-12-31 16:48:03 -0600

    Updates for lesson 2

--- Week_5/Homework/Homework_Week_5_SheCodes_Python_Advanced.py
+++ Week_5/Homework/Homework_Week_5_SheCodes_Python_Advanced.py
@@ -5,5 +5,9 @@ filename = 'Week_5\Homework\data.csv'
 with open(filename, 'r') as csvfile:
   reader = csv.DictReader(csvfile)
   for line in reader:
-    print(line['continent'])
+    continent = line['continent']
+    year = line['year']
+    population = line['population']
+    
+    
  
