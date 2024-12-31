28b5890093bd4d4a0df3e8e968c8dd26e026728d
h-schroeder-coder <hankorschroeder@gmail.com>
2024-12-31 15:59:28 -0600

    additional documentation added

--- Week_4/Lesson_5/Lesson_5.py
+++ Week_4/Lesson_5/Lesson_5.py
@@ -1,13 +1,18 @@
 import csv
 
+def display_customer_info(customer):
+  """Show data from customer csv file"""
+  customer_number = customer[0]
+  first_name = customer[2]
+  last_name = customer[3]
+  email = customer[9]
+  print(f"Customer #{customer_number}, {first_name} {last_name}, {email}")  
 
-with open('week_4/lesson_5/customer.csv', 'r') as file:
-  customers = csv.reader(file)
+
+"""Loop through each line from customer.csv file and display each customer's number, first and last name, and email"""
+with open('week_4/lesson_5/customer.csv', 'r') as customer_file:
+  customers = csv.reader(customer_file)
 
   for customer in customers:
-    customer_number = customer[0]
-    first_name = customer[2]
-    last_name = customer[3]
-    email = customer[9]
-    print(f"Customer #{customer_number}, {first_name} {last_name}, {email}")  
+    display_customer_info(customer)  
 
