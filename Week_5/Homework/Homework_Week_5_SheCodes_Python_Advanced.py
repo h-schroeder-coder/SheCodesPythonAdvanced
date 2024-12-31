71dbf211f0a44ee1709045540aef5d0c913129c6
h-schroeder-coder <hankorschroeder@gmail.com>
2024-12-31 17:12:33 -0600

    Data manipulation for Week 5 Homework

--- Week_5/Homework/Homework_Week_5_SheCodes_Python_Advanced.py
+++ Week_5/Homework/Homework_Week_5_SheCodes_Python_Advanced.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 
 filename = 'Week_5\Homework\data.csv'
 
+population_per_continent = {}
+
+
+
 with open(filename, 'r') as csvfile:
   reader = csv.DictReader(csvfile)
   for line in reader:
@@ -11,7 +15,15 @@ with open(filename, 'r') as csvfile:
     year = line['year']
     population = line['population']
     
+    if continent not in population_per_continent:
+      population_per_continent[continent] = {'population': [], 'years': []}
     
+    population_per_continent[continent]['population'].append(population)
+    population_per_continent[continent]['years'].append(year)
+    
+
+
+  
     
 plt.plot([1990, 2000, 2010, 2020], [0, 25000000, 30000000, 35000000], label="Population", marker='*', color='m')
 plt.plot([10, 20, 30], [15000000, 30000000, 40000000], label ="Animal", marker='o', color='c')
